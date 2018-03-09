e262552d2ee5ef4e3af78c5c7a61c636065e8347
Wayne Huang <wayne.huang14@gmail.com>
2018-03-08 20:30:11 -0800

    Adds static news list with search bar
    * Allows search filter
    * Button to dismiss individual news

--- src/App.js
+++ src/App.js
@@ -1,18 +1,111 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [ {
+  title: 'React',
+  url: 'https://facebook.github.io/react/',
+  author: 'Jordan Walke',
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+}, {
+  title: 'Redux',
+  url: 'https://github.com/reactjs/redux',
+  author: 'Dan Abramov, Andrew Clark',
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+}, ];
+
+function isSearched(searchTerm) {
+  return function (item) {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  }
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      list,
+      searchTerm: ''
+    };
+    
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+  }
+  
+  onDismiss(id) {
+    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    
+    this.setState({ list: updatedList });
+  }
+  
+  onSearchChange(event) {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
   render() {
+    const { searchTerm, list } = this.state;
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    
+    return (
+      <form>
+        <input
+          type='text'
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map((item) =>
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type='button'
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
       </div>
     );
   }
