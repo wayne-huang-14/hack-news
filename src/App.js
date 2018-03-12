af2c7b18fe65625de965bdabf04ee4c22147ca4a
Wayne Huang <wayne.huang14@gmail.com>
2018-03-12 12:40:55 -0700

    Fixes dismiss button to allow result state to update on hits property

--- src/App.js
+++ src/App.js
@@ -42,9 +42,9 @@ class App extends Component {
   }
   
   onDismiss(id) {
-    const updatedList = this.state.list.filter((item) => item.objectID !== id);
+    const updatedList = this.state.result.hits.filter((item) => item.objectID !== id);
     
-    this.setState({ list: updatedList });
+    this.setState({ result: {...this.state.result, hits: updatedList} });
   }
   
   onSearchChange(event) {
